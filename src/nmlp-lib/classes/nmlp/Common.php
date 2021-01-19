8ef9c213a6379b8554ae70dc53913f3a48a97282
dengiken <syssec-1@dengiken.net>
2021-01-19 11:46:43 +0900

    Commonクラスにコメント追加

--- src/nmlp-lib/classes/nmlp/Common.php
+++ src/nmlp-lib/classes/nmlp/Common.php
@@ -1,8 +1,27 @@
 <?php
+/**
+ * @file    Common.php
+ * @brief   汎用クラス
+ * @author  Dengiken
+ * @date    2021-01-19
+ */
+
 namespace nmlp;
 
+/**
+ * @package nmlp
+ * @class   Common
+ * @brief   汎用的に使用するクラス
+ */
 class Common
 {
+    /**
+     * @fn  loadCSS
+     * @brief CSSのオートローディング
+     * @access public
+     * @param void
+     * @return void
+     */
     public static function loadCSS()
     {
         $cssFile = HOME_URI . "/css" . preg_replace("/" . preg_quote(HOME_URI, "/") . "/", "", $_SERVER["SCRIPT_NAME"]);
@@ -12,6 +31,13 @@ class Common
         }
     }
 
+    /**
+     * @fn      loadCSS
+     * @brief   Javascriptのオートローディング
+     * @access  public
+     * @param   void
+     * @return  void
+     */
     public static function loadJS()
     {
         $cssFile = HOME_URI . "/js" . preg_replace("/" . preg_quote(HOME_URI, "/") . "/", "", $_SERVER["SCRIPT_NAME"]);
